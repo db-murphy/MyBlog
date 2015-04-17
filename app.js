2cb1ba570e4e01fd866e3f9c4644c3ecd7a2ca7e
K-Murphy <455272698@qq.com>
2015-04-17 15:17:11 +0800

    diwjfjowf, fix #2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+
+
 var bodyParser = require('body-parser');
 var session = require('express-session');
 
